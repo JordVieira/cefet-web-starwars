81d5aebbaccc9bc72e4fad1d8c1f58ee41d9c56f
Jordã Macena <jorda.macena@avenueeco.com>
2022-06-23 09:12:48 -0300

    finished activity

--- starwars.js
+++ starwars.js
@@ -4,4 +4,49 @@
 //  - Pegar a lista de filmes (AJAX) e preencher no HTML
 //  - Quando um filme for clicado, exibir sua introdução
 
-const API_ENDPOINT = 'https://swapi.dev/api'
+const API_ENDPOINT = "https://swapi.dev/api";
+
+import { play } from "./music.js";
+
+const songData = {
+  audioUrl: "./audio/tema-sw.mp3",
+  coverImageUrl: "./imgs/logo.svg",
+  title: "Intro",
+  artist: "John Williams",
+};
+
+play(songData, document.body);
+
+import { decimalToRoman } from "./roman.js";
+import { restartAnimation } from "./restart-animation.js";
+import { friendlyFetch } from "./friendly-fetch.js";
+
+const moviesList = document.querySelector("#filmes ul");
+moviesList.innerHTML = "";
+
+const addMovieElement = ({ episode_id, title, content }) => {
+  let newMovieElement = document.createElement("li");
+  newMovieElement.innerHTML = `Episode ${decimalToRoman(
+    episode_id
+  )} - ${title}`;
+
+  newMovieElement.addEventListener("click", () => {
+    let introductionElement = document.querySelector("pre.introducao");
+    introductionElement.innerHTML = `Episode ${decimalToRoman(
+      episode_id
+    )}\n${title}\n\n${content}`;
+    restartAnimation(introductionElement);
+  });
+
+  moviesList.appendChild(newMovieElement);
+};
+
+friendlyFetch(`${API_ENDPOINT}/films`).then((response) => {
+  let data = response.results.map(({ episode_id, title, opening_crawl }) => {
+    return { episode_id, title, content: opening_crawl };
+  });
+
+  data = data.sort((a, b) => (a.episode_id > b.episode_id ? 1 : -1));
+
+  data.forEach((data) => addMovieElement(data));
+});
